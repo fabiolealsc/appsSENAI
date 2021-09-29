a589bd6baa6890597a7bbf3a079dc4a4e2991b25
Fabio Leal Schmitz <86978853+fabiolealsc@users.noreply.github.com>
2021-09-29 00:26:32 -0300

    firsting

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace appsSENAI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
